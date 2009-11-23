73b09ccdf15ad8be91d4f36321dde234feba5ea8
Lorenzo Pierfederici <lpierfederici@gmail.com>
2009-11-23 17:07:10 +0100

    rebuilt egg info to require tg2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='',
     #url='',
     install_requires=[
-        "TurboGears2 >= 2.0b7",
+        "TurboGears2 >= 2.1a2",
         "Babel >=0.9.4",
         #can be removed iif use_toscawidgets = False
         "toscawidgets >= 0.9.7.1",
