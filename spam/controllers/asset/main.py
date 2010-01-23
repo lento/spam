268c3a2104a3f438c7157589b8d508012f4f5517
Lorenzo Pierfederici <lpierfederici@gmail.com>
2010-01-23 17:10:00 +0100

    fix asset history after thumb_path -> thumbnail rename

--- spam/controllers/asset/main.py
+++ spam/controllers/asset/main.py
@@ -102,18 +102,18 @@ class Controller(RestController):
         for ver in asset.versions:
             if ver.notes:
                 for note in ver.notes:
-                    history.append(dict(id=None, proj_id=None, thumb_path=None,
+                    history.append(dict(id=None, proj_id=None, thumbnail=None,
                                     ver=None, fmtver=None, header=note.header,
                                     text=note.text, lines=note.lines))
             else:
-                history.append(dict(id=None, proj_id=None, thumb_path=None,
+                history.append(dict(id=None, proj_id=None, thumbnail=None,
                                     ver=None, fmtver=None, header='',
                                     text='', lines=[]))
             
             history[-1]['id'] = ver.id
             history[-1]['proj_id'] = ver.asset.proj_id
             history[-1]['has_preview'] = ver.has_preview
-            history[-1]['thumb_path'] = ver.thumb_path
+            history[-1]['thumbnail'] = ver.thumbnail
             history[-1]['ver'] = ver.ver
             history[-1]['fmtver'] = ver.fmtver
         
