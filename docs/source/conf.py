2800a4986910b71cd3df80275caf42471d79b812
Lorenzo Pierfederici <lpierfederici@gmail.com>
2010-09-02 20:17:33 -0700

    docs: use sphinx extension to prepare docs for github pages
    thanks to http://github.com/michaeljones/sphinx-to-github

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
-              'spam.commands.pylons_autodoc']
+              'spam.commands.pylons_autodoc', 'sphinxtogithub']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,7 +85,7 @@ exclude_trees = []
 pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
+modindex_common_prefix = ['spam.']
 
 
 # -- Options for HTML output ---------------------------------------------------
